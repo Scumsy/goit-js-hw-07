abfdb2dfba96aed7bd3bf2bad2ab968797074374
Scumsy <dmitry.koschavka@gmail.com>
2022-08-12 14:15:52 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,24 +21,16 @@ const galleryList = galleryItems.map(({ preview, original, description }) => {
 const imageGallery = document.querySelector('.gallery');
 
 imageGallery.insertAdjacentHTML('beforeend', galleryList.join(''));
-// const openModalImage = basicLightbox.create(`
-//   	<div class = 'modal'><img
-//         class="modal__image"
-//         src="${evt.target.dataset.source}"
-//         data-source="${evt.target.dataset.source}"
-//         alt=""
-//       /></div>
-//   `);
 
 imageGallery.addEventListener('click', evt => {
   evt.preventDefault();
-  //   console.log(evt.target.dataset.source);
+
   const instance = basicLightbox.create(`
   	<div class = 'modal'><img
-        class="modal__image"
+        
         src="${evt.target.dataset.source}"
-        data-source="${evt.target.dataset.source}"
-        alt=""
+        
+        alt="${description}"
       /></div>
   `);
 
