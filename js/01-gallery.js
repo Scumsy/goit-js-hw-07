363572bf8836f133b71e003cf6960506b25deefc
Scumsy <dmitry.koschavka@gmail.com>
2022-08-12 14:14:36 +0300

    hw-1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+//__________________________ -- 1
+const galleryList = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+});
+
+//______________________________-- 2
+const imageGallery = document.querySelector('.gallery');
+
+imageGallery.insertAdjacentHTML('beforeend', galleryList.join(''));
+// const openModalImage = basicLightbox.create(`
+//   	<div class = 'modal'><img
+//         class="modal__image"
+//         src="${evt.target.dataset.source}"
+//         data-source="${evt.target.dataset.source}"
+//         alt=""
+//       /></div>
+//   `);
+
+imageGallery.addEventListener('click', evt => {
+  evt.preventDefault();
+  //   console.log(evt.target.dataset.source);
+  const instance = basicLightbox.create(`
+  	<div class = 'modal'><img
+        class="modal__image"
+        src="${evt.target.dataset.source}"
+        data-source="${evt.target.dataset.source}"
+        alt=""
+      /></div>
+  `);
+
+  console.log(instance.show());
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+});
+
+// imageGallery.addEventListener('click', evt => {
+//   evt.preventDeafaults();
+//   const instance = basicLightbox.create(`
+//   	<div class = 'modal'><img
+//         class="modal__image"
+//         src="${evt.target.dataset.source}"
+//         data-source="${evt.target.dataset.source}"
+//         alt=""
+//       /></div>`);
+//   console.log(instance.show());
+// });
