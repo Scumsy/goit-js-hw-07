267e0e05f7a7e55b690c2009e4beadb4ff6cb412
Scumsy <dmitry.koschavka@gmail.com>
2022-08-13 12:55:06 +0300

    HW-7 done with ESC

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-//__________________________ -- 1
-const galleryList = galleryItems.map(({ preview, original, description }) => {
-  return `<div class="gallery__item">
+const galleryListItem = galleryItems.map(
+  ({ preview, original, description }) => {
+    return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -15,24 +13,66 @@ const galleryList = galleryItems.map(({ preview, original, description }) => {
     />
   </a>
 </div>`;
-});
+  },
+);
 
-//______________________________-- 2
 const imageGallery = document.querySelector('.gallery');
 
-imageGallery.insertAdjacentHTML('beforeend', galleryList.join(''));
+imageGallery.insertAdjacentHTML('beforeend', galleryListItem.join(''));
 
-imageGallery.addEventListener('click', evt => {
+//------------------------------------------
+
+function modalImageUsage(evt) {
   evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox
+    .create(
+      `
+      	<div class = 'modal'><img
 
-  const instance = basicLightbox.create(`
-  	<div class = 'modal'><img
-        
-        src="${evt.target.dataset.source}"
-        
-        alt="${evt.target.dataset.alt}"
-      /></div>
-  `);
+            src="${evt.target.dataset.source}"
 
-  console.log(instance.show());
-});
+            alt="${evt.target.dataset.alt}"
+          /></div>
+      `,
+      {
+        onShow: instance =>
+          window.addEventListener('keydown', evt => {
+            if (evt.code === 'Escape') {
+              console.log('Exit the gallery');
+              return instance.close();
+            }
+          }),
+      },
+      { onClose: instance => window.removeEventListener('keydown', instance) },
+    )
+    .show();
+}
+
+imageGallery.addEventListener('click', modalImageUsage);
+
+// -----------------------------
+
+// imageGallery.addEventListener('click', modalImageOpen);
+
+// const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
+//   onShow: instance => window.addEventListener('keydown', onEscExit),
+//   onClose: instance => window.removeEventListener('keydown', onEscExit),
+// });
+// function modalImageOpen(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   instance.element().querySelector('modal-img').src = evt.target.dataset.source;
+//   instance.show();
+// }
+
+// function onEscExit(evt) {
+//   if (evt.code === 'Escape') {
+//     console.log('Exit the gallery');
+//     return instance.close();
+//   }
+// }
