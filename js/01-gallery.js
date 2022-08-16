20f1f912cb51c1ad4d5a2f5100b99f2a65a191bd
Scumsy <dmitry.koschavka@gmail.com>
2022-08-16 08:43:02 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,57 +22,59 @@ imageGallery.insertAdjacentHTML('beforeend', galleryListItem.join(''));
 
 //------------------------------------------
 
-function modalImageUsage(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  const instance = basicLightbox
-    .create(
-      `
-      	<div class = 'modal'><img
-
-            src="${evt.target.dataset.source}"
-
-            alt="${evt.target.dataset.alt}"
-          /></div>
-      `,
-      {
-        onShow: instance =>
-          window.addEventListener('keydown', evt => {
-            if (evt.code === 'Escape') {
-              console.log('Exit the gallery');
-              return instance.close();
-            }
-          }),
-      },
-      { onClose: instance => window.removeEventListener('keydown', instance) },
-    )
-    .show();
-}
-
-imageGallery.addEventListener('click', modalImageUsage);
-
-// -----------------------------
-
-// imageGallery.addEventListener('click', modalImageOpen);
-
-// const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
-//   onShow: instance => window.addEventListener('keydown', onEscExit),
-//   onClose: instance => window.removeEventListener('keydown', onEscExit),
-// });
-// function modalImageOpen(evt) {
+// function modalImageUsage(evt) {
 //   evt.preventDefault();
 //   if (!evt.target.classList.contains('gallery__image')) {
 //     return;
 //   }
-//   instance.element().querySelector('modal-img').src = evt.target.dataset.source;
-//   instance.show();
+//   const instance = basicLightbox
+//     .create(
+//       `
+//       	<div class = 'modal'><img
+
+//             src="${evt.target.dataset.source}"
+
+//             alt="${evt.target.dataset.alt}"
+//           /></div>
+//       `,
+//       {
+//         onShow: instance => window.addEventListener('keydown', onEscExit),
+//       },
+//       { onClose: instance => window.removeEventListener('keydown', onEscExit) },
+//     )
+//     .show();
 // }
 
 // function onEscExit(evt) {
 //   if (evt.code === 'Escape') {
 //     console.log('Exit the gallery');
-//     return instance.close();
+//     instance.close();
+//     return;
 //   }
 // }
+
+// imageGallery.addEventListener('click', modalImageUsage);
+
+// -----------------------------
+
+imageGallery.addEventListener('click', modalImageOpen);
+
+const instance = basicLightbox.create(`<img class="modal-img" src="">`, {
+  onShow: instance => window.addEventListener('keydown', onEscExit),
+  onClose: instance => window.removeEventListener('keydown', onEscExit),
+});
+function modalImageOpen(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  instance.element().querySelector('img').src = evt.target.dataset.source;
+  instance.show();
+}
+
+function onEscExit(evt) {
+  if (evt.code === 'Escape') {
+    console.log('Exit the gallery');
+    return instance.close();
+  }
+}
